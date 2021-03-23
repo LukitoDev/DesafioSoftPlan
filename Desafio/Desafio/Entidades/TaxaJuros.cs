dab38dccd761248c2debf39fe589e08852ca6cb2
LucasOrtolan <37518421+LucasOrtolan@users.noreply.github.com>
2021-03-23 20:15:06 -0300

    Start
    Start

---
+++ Desafio/Desafio/Entidades/TaxaJuros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Entidades
+{
+    public class TaxaJuros
+    {
+        private static readonly double _valor = 0.01;
+
+        public static double Valor
+        {
+            get => _valor;
+        }
+
+        public static double CalculaJuros(double valorInicial, int meses)
+        {
+            return Math.Pow(1 + _valor, meses) * valorInicial;
+        }
+    }
+}
